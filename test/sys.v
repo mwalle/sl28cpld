dd20045930223e2c2f10137d05d8c54089c02751
Michael Walle <michael@walle.cc>
2020-07-13 00:35:09 +0200

    add basic sys testbench
    Signed-off-by: Michael Walle <michael@walle.cc>

---
+++ test/sys.v
@@ -0,0 +1,25 @@
+reg rst;
+reg clk;
+
+initial clk = 1'b0;
+always #100 clk = ~clk;
+
+task waitclock;
+begin
+	@(posedge clk)
+	#1;
+end
+endtask
+
+task waitreset;
+begin
+	@(negedge rst)
+	#1;
+end
+endtask
+
+initial begin
+	rst = 1'b1;
+	waitclock;
+	rst = 1'b0;
+end
