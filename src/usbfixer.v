b53dd08f8dfac23bcdc331d76acb12f388975516
Michael Walle <michael@walle.cc>
2020-07-15 01:25:40 +0200

    usbfixer: keep output disabled if pwrfault is asserted
    Signed-off-by: Michael Walle <michael@walle.cc>

--- src/usbfixer.v
+++ src/usbfixer.v
@@ -9,6 +9,6 @@ always @(negedge usb_en_oc_n, negedge usb_drvvbus)
 		usb_pwrfault <= 1'b0;
 	else
 		usb_pwrfault <= 1'b1;
-assign usb_en_oc_n = usb_drvvbus ? 1'bz : 1'b0;
+assign usb_en_oc_n = (usb_drvvbus & !usb_pwrfault) ? 1'bz : 1'b0;
 
 endmodule
