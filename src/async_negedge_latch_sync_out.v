05b7664d54b243c9157532af7ab067b7a3758c25
Michael Walle <michael@walle.cc>
2020-08-12 18:05:35 +0200

    reset_req: move the latch into its own module
    The latch will be reused by the power-off circuit. Also add an enable
    input, this will be used to either enable the reset or the power-off
    circuit.
    
    Signed-off-by: Michael Walle <michael@walle.cc>

---
+++ src/async_negedge_latch_sync_out.v
@@ -0,0 +1,28 @@
+module async_negedge_latch_sync_out (
+	input clk,
+
+	input in,
+	input enable,
+	input clear,
+	output reg async_out,
+	output sync_out,
+	output sync_out_edge
+);
+
+initial async_out = 1'b0;
+always @(negedge in, posedge clear) begin
+	if (clear)
+		async_out <= 1'b0;
+	else if (enable)
+		async_out <= 1'b1;
+end
+
+sync_edge sync_edge_latch (
+	.clk(clk),
+
+	.in(async_out),
+	.out(sync_out),
+	.out_posedge(sync_out_edge)
+);
+
+endmodule
--- src/reset_req.v
+++ src/reset_req.v
@@ -5,30 +5,26 @@ module reset_req #(
 	input ce,
 
 	input reset_req_n,
-	output reg reset_req_out
+	input enable,
+	output reset_req_out
+);
+
+wire reset_req_sync;
+async_negedge_latch_sync_out latch (
+	.clk(clk),
+
+	.in(reset_req_n),
+	.enable(enable),
+	.clear(clear),
+	.async_out(reset_req_out),
+	.sync_out(reset_req_sync)
 );
 
 reg clear;
-initial reset_req_out = 1'b0;
-always @(negedge reset_req_n, posedge clear) begin
-	if (clear)
-		reset_req_out <= 1'b0;
-	else
-		reset_req_out <= 1'b1;
-end
-
-wire reset_req_s;
-sync_edge sync_edge_reset_req (
-	.clk(clk),
-
-	.in(reset_req_out),
-	.out(reset_req_s)
-);
-
 reg [2:0] reset_req_cnt;
 always @(posedge clk) begin
 	clear <= 1'b0;
-	if (!reset_req_s)
+	if (!reset_req_sync)
 		reset_req_cnt <= 3'b0;
 	else if (reset_req_cnt == EXTEND_COUNT)
 		clear <= 1'b1;
